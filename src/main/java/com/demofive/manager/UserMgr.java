a94672e30fd740c549deb4278cc8c2b6fb30a715
martin <mandalahua@163.com>
2019-05-13 12:10:29 +0800

    dubbo接口

---
+++ src/main/java/com/demofive/manager/UserMgr.java
@@ -0,0 +1,14 @@
+package com.demofive.manager;
+
+import com.demofive.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by Martin on 2019/5/12.
+ */
+public interface UserMgr {
+    List<User> listAllUser();
+    Integer saveUser(User user);
+    Integer countUser();
+}
